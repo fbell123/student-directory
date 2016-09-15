aaea59817fbb8d56a2916422c80e2710b89dc1ec
Frankie Bell <fbell.pro@gmail.com>
2016-09-15 11:51:47 +0100

    part 9 completed with the plural and singular sections added, applied two versions of the method for practice

--- directory.rb
+++ directory.rb
@@ -1,6 +1,4 @@
 def input_students
-  puts "Please enter the name of the student."
-  puts "Hit return twice to exit."
   # create an empty array
   students = []
 
@@ -14,14 +12,29 @@ def input_students
       puts "Please enter the cohort they are joining (November as default)"
       cohort = gets.chomp
 
-      if cohort.empty?
-        cohort = "November" if cohort.empty?
-      end
+        puts "Please enter any hobbies"
+        hobbies = gets.chomp
+
+        puts "Please enter country of birth"
+        birth = gets.chomp
+
+        puts "Please enter height (cm)"
+        height = gets.chomp
+
+        puts "Please enter weight (kg)"
+        weight = gets.chomp
+  
 
   # while the name is not empty, repeat this code
     # add the student hash to array
-    students << {name: name, cohort: cohort}
-    puts "Now we have #{students.count} students."
+    students << {name: name,
+      cohort: cohort,
+      hobbies: hobbies,
+      birth: birth,
+      height: height,
+      weight: weight}
+
+    puts plural_singular(students)
     # get another name from the user
     name = gets.chomp
   end
@@ -35,12 +48,26 @@ end
 
 def print_default(students)
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort),
+    born in #{student[:birth]}, currently #{student[:height]} cm tall, weighing #{student[:weight]} kg,
+    enjoys #{student[:hobbies]} as a hobby."
+  end
+end
+
+def plural_singular(students)
+  if students.count > 1
+    puts "Now we have #{students.count} students."
+  elsif students.count == 1
+    puts "Now we have #{students.count} student."
   end
 end
 
 def print_footer(students)
+  if students.count == 1
+  puts "Overall, we have #{students.count} great student"
+else
   puts "Overall, we have #{students.count} great students"
+end
 end
 
 
