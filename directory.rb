c162df807aa3aac8b8df7a9d654ee0593b8c1f47
Frankie Bell <fbell.pro@gmail.com>
2016-09-15 11:05:41 +0100

    changed the while loop to include the extra information required and also for assinging a default value (just for the cohort)

--- directory.rb
+++ directory.rb
@@ -1,40 +1,50 @@
 def input_students
-  puts "Please enter the name of the students."
-  puts "To finish, return through the questions."
+  puts "Please enter the name of the student."
+  puts "Hit return twice to exit."
   # create an empty array
   students = []
-  # get the first name
-  name = gets.chomp
+
+  while true do
+
+    puts "Please enter the name of the student or hit return to exit."
+    name = gets.chomp
+
+      break if name.empty?
+
+      puts "Please enter the cohort they are joining (November as default)"
+      cohort = gets.chomp
+
+      if cohort.empty?
+        cohort = "November" if cohort.empty?
+      end
 
   # while the name is not empty, repeat this code
-  while !name.empty? do
     # add the student hash to array
-    students << {name: name, cohort: :November}
+    students << {name: name, cohort: cohort}
     puts "Now we have #{students.count} students."
     # get another name from the user
     name = gets.chomp
   end
-
     students
 end
 
 def print_header
-puts "The students of Villain Academy".center(40)
-puts "-----------".center(40)
+puts "The students of Villain Academy"
+puts "-----------"
 end
 
-def print(students)
+def print_default(students)
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(40)
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(40)
+  puts "Overall, we have #{students.count} great students"
 end
 
 
 students = input_students
 print_header
-print(students)
+print_default(students)
 print_footer(students)
